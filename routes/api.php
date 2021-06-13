f87a442a8ec2d133a5efbf813c10ec5789ae3220
alvaromoli13 <soulalvaro@gmail.com>
2021-06-13 23:22:49 +0200

    Token

--- routes/api.php
+++ routes/api.php
@@ -17,38 +17,34 @@ use Illuminate\Http\Request;
 Route::post('register', 'API\RegisterController@register');
 Route::post('login', 'API\RegisterController@login');
 
-Route::resource('sagas', 'SagaController');
-
-Route::resource('comentarios', 'ComentarioController');
-Route::get('comentariosPeli/{idPelicula}', 'ComentarioController@ComentarioAsociadoPelicula');
-
-Route::resource('personajes', 'PersonajeController');
-
-Route::resource('peliculas', 'PeliculaController');
-
-Route::resource('meGustaPersonajes', 'UsuarioPersonajeController');
-Route::get('meGustaPersonaje/{idPersonaje}', 'UsuarioPersonajeController@meGustasTotales');
-Route::delete('eliminarMPersonaje/{idPersonaje}.{idUsuario}', 'UsuarioPersonajeController@eliminar');
-
-Route::resource('meGustaPeliculas', 'UsuarioPeliculaController');
-Route::get('meGustaPelicula/{idPelicula}', 'UsuarioPeliculaController@meGustasTotales');
-Route::delete('eliminarMPeli/{idPelicula}.{idUsuario}', 'UsuarioPeliculaController@eliminar');
-
-Route::resource('guardadoPersonajes', 'PersonajeGuardadoController');
-Route::get('guardadoPersonaje/{idPersonaje}', 'PersonajeGuardadoController@guardadosTotales');
-Route::delete('eliminarGPersonaje/{idPersonaje}.{idUsuario}', 'PersonajeGuardadoController@eliminar');
-Route::get('personajesAsociados/{idUsuario}', 'PersonajeGuardadoController@personajesAsociados');
-
-
-Route::resource('guardadoPeliculas', 'PeliculasGuardadasController');
-Route::get('guardadoPelicula/{idPelicula}', 'PeliculasGuardadasController@guardadosTotales');
-Route::delete('eliminarGPelicula/{idPelicula}.{idUsuario}', 'PeliculasGuardadasController@eliminar');
-Route::get('peliculasAsociadas/{idUsuario}', 'PeliculasGuardadasController@peliculasAsociadas');
-
-
-
-
 
 Route::middleware('auth:api')->group( function () {
-    
+    Route::resource('sagas', 'SagaController');
+
+    Route::resource('comentarios', 'ComentarioController');
+    Route::get('comentariosPeli/{idPelicula}', 'ComentarioController@ComentarioAsociadoPelicula');
+
+    Route::resource('personajes', 'PersonajeController');
+
+    Route::resource('peliculas', 'PeliculaController');
+
+    Route::resource('meGustaPersonajes', 'UsuarioPersonajeController');
+    Route::get('meGustaPersonaje/{idPersonaje}', 'UsuarioPersonajeController@meGustasTotales');
+    Route::delete('eliminarMPersonaje/{idPersonaje}.{idUsuario}', 'UsuarioPersonajeController@eliminar');
+
+    Route::resource('meGustaPeliculas', 'UsuarioPeliculaController');
+    Route::get('meGustaPelicula/{idPelicula}', 'UsuarioPeliculaController@meGustasTotales');
+    Route::delete('eliminarMPeli/{idPelicula}.{idUsuario}', 'UsuarioPeliculaController@eliminar');
+
+    Route::resource('guardadoPersonajes', 'PersonajeGuardadoController');
+    Route::get('guardadoPersonaje/{idPersonaje}', 'PersonajeGuardadoController@guardadosTotales');
+    Route::delete('eliminarGPersonaje/{idPersonaje}.{idUsuario}', 'PersonajeGuardadoController@eliminar');
+    Route::get('personajesAsociados/{idUsuario}', 'PersonajeGuardadoController@personajesAsociados');
+
+
+    Route::resource('guardadoPeliculas', 'PeliculasGuardadasController');
+    Route::get('guardadoPelicula/{idPelicula}', 'PeliculasGuardadasController@guardadosTotales');
+    Route::delete('eliminarGPelicula/{idPelicula}.{idUsuario}', 'PeliculasGuardadasController@eliminar');
+    Route::get('peliculasAsociadas/{idUsuario}', 'PeliculasGuardadasController@peliculasAsociadas');
+
 });
